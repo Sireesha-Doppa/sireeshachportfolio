bfcd9461ba6627d70967b24c4523aff9af20f728
Sireesha-Doppa <doppasirisha@gmail.com>
2023-10-16 21:11:27 +0530

    done

---
+++ portfolio.js
@@ -0,0 +1,6 @@
+function displayText() {
+    var textElement = document.getElementById("text");
+    textElement.textContent ="In this static portfolio related to showcase my skills using HTML,
+    CSS,Bootstrap";
+    textElement.style.display = "block";
+  }
