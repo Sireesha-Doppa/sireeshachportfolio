193e2bdde1ad0b3a6ba0fc075978f3fbe47a8979
Sireesha-Doppa <doppasirisha@gmail.com>
2023-10-22 16:05:50 +0530

    done

--- portfolio.js
+++ portfolio.js
@@ -1,11 +1,11 @@
-$(document).ready(function(){
+(document).ready(function($){
     $("a").on('click', function(event) {
       if (this.hash !== "") {
         event.preventDefault();
         var hash = this.hash;
         $('html, body').animate({
           scrollTop: $(hash).offset().top
-        }, 800, function(){
+        },400, function(){
           window.location.hash = hash;
         });
       }
